fe14a054290aa13ca0cd13f810dc43c42dce009b
NgoDuc <ngoduc202@gmail.com>
2023-09-29 15:29:47 +0700

    Update: auth

--- src/routes/commentRoute/commentRoute.js
+++ src/routes/commentRoute/commentRoute.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import { postCommentHandler, getPostCommentHandler, updateContentHandler, deleteCommentHandler } from '../../controllers/commentController/commentController.js'
+import { authentication, checkUserDidLogin } from '../../services/jwt/JWTservices.js'
 
 const commentRoute = express.Router()
+commentRoute.all('*',checkUserDidLogin,authentication)
+
 commentRoute.post('/postComment/:postId', postCommentHandler)
 commentRoute.get('/getPostComment/:postId', getPostCommentHandler)
 commentRoute.put('/updateContent/:commentId', updateContentHandler)
--- src/routes/roleRoute/roleRoute.js
+++ src/routes/roleRoute/roleRoute.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import { getRoleHandler, getRoleByIDHandler } from '../../controllers/roleController/roleController.js'
+import { authentication, checkUserDidLogin } from '../../services/jwt/JWTservices.js'
 
 const roleRoute = express.Router()
+roleRoute.all('*',checkUserDidLogin,authentication)
+
 roleRoute.get('/getRole', getRoleHandler)
 roleRoute.get('/getRoleByID/:idRole', getRoleByIDHandler)
 export default roleRoute
